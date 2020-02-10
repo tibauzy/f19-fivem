9eadfe8398e2f83717575d33421726ff1b321619
tibauzy <60845893+tibauzy@users.noreply.github.com>
2020-02-10 15:12:48 +0300

    Add files via upload

---
+++ languages.ts
@@ -0,0 +1,74 @@
+const languages = [
+    {
+        name: 'en',
+        displayName: 'English'
+    },
+    {
+        name: 'fr',
+        displayName: 'Français'
+    },
+    {
+        name: 'nl',
+        displayName: 'Nederlands'
+    },
+    {
+        name: 'da',
+        displayName: 'Dansk'
+    },
+    {
+        name: 'de',
+        displayName: 'Deutsch'
+    },
+    {
+        name: 'pl',
+        displayName: 'Polski'
+    },
+    {
+        name: 'it',
+        displayName: 'Italiano'
+    },
+    {
+        name: 'pt',
+        displayName: 'Português'
+    },
+    {
+        name: 'ru',
+        displayName: 'Русский'
+    },
+    {
+        name: 'es',
+        displayName: 'Español'
+    },
+    {
+        name: 'cs',
+        displayName: 'Čeština'
+    },
+	 {
+        name: 'tr',
+        displayName: 'Türkçe'
+    },
+];
+
+export class Languages {
+    static toList() {
+        return languages.map(({name}) => ({
+            code: name,
+            dir: 'ltr'
+        }));
+    }
+
+    static toSettingsOptions() {
+        const options: {[code: string]: string} = {};
+
+        const languagesSet = languages;
+        languagesSet.sort((a, b) => {
+            return a.displayName.localeCompare(b.displayName);
+        });
+
+        for (const lang of languagesSet) {
+            options[lang.name] = lang.displayName;
+        }
+
+        return options;
+    }
+}
